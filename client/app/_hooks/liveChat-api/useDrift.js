8b22c808608785738f35d7c9f01d8ff6ff9b7182
jesus doza <jesus.doza@gmail.com>
2024-05-08 19:12:52 -0500

    remove env.local from git

--- client/app/_hooks/liveChat-api/useDrift.js
+++ client/app/_hooks/liveChat-api/useDrift.js
@@ -1,15 +1,17 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
+import { useEffect } from "react";
+const driftKey = process.env.NEXT_PUBLIC_DRIFT_KEY;
 
 const useDrift = () => {
+  console.log("driftKey", driftKey);
   useEffect(() => {
     // Avoid duplicate loading
     if (window.drift) return;
 
     // Initialization script
-    (function() {
-      var t = window.driftt = window.drift = window.driftt || [];
+    (function () {
+      var t = (window.driftt = window.drift = window.driftt || []);
       if (!t.init) {
         if (t.invoked) {
           if (window.console && console.error) {
@@ -19,22 +21,33 @@ const useDrift = () => {
         }
         t.invoked = true;
         t.methods = [
-          "identify", "config", "track", "reset", "debug", "show",
-          "ping", "page", "hide", "off", "on"
+          "identify",
+          "config",
+          "track",
+          "reset",
+          "debug",
+          "show",
+          "ping",
+          "page",
+          "hide",
+          "off",
+          "on",
         ];
-        t.factory = function(e) {
-          return function() {
+        t.factory = function (e) {
+          return function () {
             var n = Array.prototype.slice.call(arguments);
             n.unshift(e);
             t.push(n);
             return t;
           };
         };
-        t.methods.forEach(function(e) {
+        t.methods.forEach(function (e) {
           t[e] = t.factory(e);
         });
-        t.load = function(t) {
-          var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+        t.load = function (t) {
+          var e = 3e5,
+            n = Math.ceil(new Date() / e) * e,
+            o = document.createElement("script");
           o.type = "text/javascript";
           o.async = true;
           o.crossorigin = "anonymous";
@@ -44,8 +57,9 @@ const useDrift = () => {
         };
       }
     })();
-    drift.SNIPPET_VERSION = '0.3.1';
-    drift.load('k9sf2yvdayey');
+    drift.SNIPPET_VERSION = "0.3.1";
+    // drift.load("k9sf2yvdayey");
+    drift.load(driftKey);
 
     // Cleanup function to remove script if component unmounts
     return () => {
